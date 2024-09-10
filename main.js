ca9837b31d114c282b036c0b23d1675bdec0d64d
Jonas <jonas.weimann04@gmail.com>
2024-09-10 13:47:32 -0300

    Haciendo dropdown para historiales

--- main.js
+++ main.js
@@ -1,27 +1,17 @@
 let gastos = []
-
-let categoriasGastos = []
-let categoriasIngresos = []
-
-
-let totalGastos = 0
-let totalIngresos = 0
-let totalAhorros = 0
-let saldoNeto = 0
+let ingresos = []
+let ahorros = []
 
 const botonRegistrarGasto = document.getElementById('boton--registrar-gasto')
 const botonRegistrarIngreso = document.getElementById('boton--registrar-ingreso')
 const botonRegistrarAhorro = document.getElementById('boton--registrar-ahorro')
 const historialContainer = document.querySelector('.historiales')
 
-let fechaGasto
-let detalleGasto
-let categoriaGasto
-let medioGasto
-let montoGasto
-
-let historialGastosTabla
-let historialGastosTablaBody
+let fecha
+let detalle
+let categoria
+let medio
+let monto
 
 class Gasto{
     constructor(fecha,detalle,categoria,medio,monto){
@@ -32,40 +22,111 @@ class Gasto{
         this.monto = monto
     }
 }
-
-function iniciarHistorialGastos(){
-    historialContainer.innerHTML+="<table class=historial id=historial-gastos>"
-    historialGastosTabla = document.getElementById('historial-gastos')
-    historialGastosTabla.innerHTML+='<thead><tr><th>Fecha<th>Detalle<th>Categoría<th>Medio de pago<th>Monto<tbody id=historial-gastos-body>'
-    historialGastosTablaBody = document.getElementById('historial-gastos-body')
+class Ingreso{
+    constructor(fecha,detalle,categoria,medio,monto){
+        this.fecha = fecha
+        this.detalle = detalle
+        this.categoria = categoria
+        this.medioDePago = medio
+        this.monto = monto
+    }
+}
+class Ahorro{
+    constructor(fecha,detalle,categoria,medio,monto){
+        this.fecha = fecha
+        this.detalle = detalle
+        this.categoria = categoria
+        this.medioDePago = medio
+        this.monto = monto
+    }
 }
 
-function obtenerDatos(){
-    //Obtengo los datos de los inputs del form de registro de gastos
-    fechaGasto = document.getElementById('gasto--fecha').value
-    detalleGasto = document.getElementById('gasto--detalle').value.toUpperCase()
-    categoriaGasto = document.getElementById('gasto--categoria').value.toUpperCase()
-    medioGasto = document.getElementById('gasto--medio').value.toUpperCase()
-    montoGasto = document.getElementById('gasto--monto').value
+let nosequehacerBODY =[]
+let nosequehacerCATEGORIAS =[]
+let nosequehacerTITULOS =[]
+
+function iniciarHistorial(tipo){
+    //Crea una tabla donde se añadirán los ingresos
+    historialContainer.innerHTML+=`<table class=historial id=historial-${tipo}>`
+    historial = document.getElementById(`historial-${tipo}`)
+    historial.innerHTML+=`<thead><tr><th class=historial-titulo id=historial-${tipo}-titulo>Historial De ${tipo} <tr class=historial-categorias id=historial-${tipo}-categorias><th>Fecha<th>Detalle<th>Categoría<th>Medio de pago<th>Monto<tbody id=historial-${tipo}-body>`
+    historialTitulo = document.getElementById(`historial-${tipo}-titulo`)
+    nosequehacerTITULOS.push(historialTitulo)
+    historialCategorias = document.getElementById(`historial-${tipo}-categorias`)
+    nosequehacerCATEGORIAS.push(historialCategorias)
+    historialBody = document.getElementById(`historial-${tipo}-body`)
+    nosequehacerBODY.push(historialBody)
 }
 
-function agregarGastoAArray(){
-    //Añado el nuevo gasto al array de gastos
-    let nuevoGasto = new Gasto(fechaGasto,detalleGasto,categoriaGasto,medioGasto,montoGasto)
-    gastos.push(nuevoGasto)
-    console.log(gastos)
+function obtenerDatos(tipo){
+    //Obtiene los datos de los inputs del form de registro
+    fecha = document.getElementById(`${tipo}--fecha`).value
+    detalle = document.getElementById(`${tipo}--detalle`).value.toUpperCase()
+    categoria = document.getElementById(`${tipo}--categoria`).value.toUpperCase()
+    medio = document.getElementById(`${tipo}--medio`).value.toUpperCase()
+    monto = document.getElementById(`${tipo}--monto`).value
 }
 
-function agregarGastoATabla(fechaGasto,detalleGasto,categoriaGasto,medioGasto,montoGasto){
-    historialGastosTablaBody.innerHTML+=`<tr><td>${fechaGasto}<td>${detalleGasto}<td>${categoriaGasto}<td>${medioGasto}<td>${montoGasto}`
+function clasificarDato(tipo) {
+    function crearYAgregar(Constructor, array) {
+        array.push(new Constructor(fecha, detalle, categoria, medio, monto))
+        console.log(array)
+    }
+    if (tipo == 'gastos') {
+        crearYAgregar(Gasto, gastos)
+    } else if (tipo == 'ingresos') {
+        crearYAgregar(Ingreso, ingresos)
+    } else if (tipo == 'ahorros') {
+        crearYAgregar(Ahorro, ahorros)
+    }
 }
+
+function agregarFilaAlHistorial(tipo){
+    const nuevaFila = document.createElement('tr')
+    const celdas = [fecha, detalle, categoria, medio, `$${monto}`]
+    celdas.forEach(contenido => {
+        const celda = document.createElement('td')
+        celda.innerText = contenido
+        nuevaFila.appendChild(celda)
+    });
+    historialBody = document.getElementById(`historial-${tipo}-body`)
+    historialBody.appendChild(nuevaFila)
+}
+
+function mostrarOcultarFila(){
+    nosequehacerTITULOS[0].onclick = ()=>{
+        nosequehacerCATEGORIAS[0].classList.toggle('hidden')
+        nosequehacerBODY[0].classList.toggle('hidden')
+        console.log(nosequehacerBODY,nosequehacerCATEGORIAS,nosequehacerTITULOS)
+
+    }
+    nosequehacerTITULOS[1].onclick = ()=>{
+        nosequehacerCATEGORIAS[1].classList.toggle('hidden')
+        nosequehacerBODY[1].classList.toggle('hidden')
+    }
+    nosequehacerTITULOS[2].onclick = ()=>{
+        nosequehacerCATEGORIAS[2].classList.toggle('hidden')
+        nosequehacerBODY[2].classList.toggle('hidden')
+    }
+}
+
+function agregarDato(tipo,array){
+    if (array.length == 0){
+        iniciarHistorial(tipo, array)
+    }
+    obtenerDatos(tipo)
+    clasificarDato(tipo)
+    agregarFilaAlHistorial(tipo)
+    mostrarOcultarFila()
+}
+
 
 botonRegistrarGasto.addEventListener('click',()=>{
-    if (gastos.length == 0){
-        iniciarHistorialGastos()
-    }
-    obtenerDatos()
-    agregarGastoAArray()
-    agregarGastoATabla(fechaGasto,detalleGasto,categoriaGasto,medioGasto,montoGasto)
+    agregarDato('gastos', gastos)
 })
-
+botonRegistrarIngreso.addEventListener('click',()=>{
+    agregarDato('ingresos', ingresos)
+})
+botonRegistrarAhorro.addEventListener('click',()=>{
+    agregarDato('ahorros', ahorros)
+})
